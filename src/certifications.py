8fc9027a9a93a685f45542a7a96d07da908989e4
Carl J. Nobile <carl.nobile@gmail.com>
2025-07-18 13:30:53 -0400

    More updates mostly to SQL statements.

--- src/certifications.py
+++ src/certifications.py
@@ -1,9 +1,7 @@
-import datetime
-import sqlite3
-import os
-import csv
+# -*- coding: utf-8 -*-
 
-from . import members
+import datetime
+
 from . import utils
 
 from enum import IntEnum
@@ -18,7 +16,7 @@ class CertificationLevels(IntEnum):
     CERTIFIER = 40
 
 
-class ToolUser(object):
+class ToolUser:
     def __init__(self, displayName, barcode):
         self.tools = {}
         self.displayName = displayName
@@ -27,8 +25,10 @@ class ToolUser(object):
     def addTool(self, tool_id, date, level):
         if not date:
             date = datetime.datetime(2019, 1, 1)
+
         if tool_id in self.tools:
-            (currDate, nil) = self.tools[tool_id]
+            currDate, nil = self.tools[tool_id]
+
             if date > currDate:
                 self.tools[tool_id] = (date, level)
         else:
@@ -41,9 +41,8 @@ class ToolUser(object):
             return ("", CertificationLevels.NONE)
 
     def getHTMLCellTool(self, tool_id):
-        (dateObj, level) = self.getTool(tool_id)
+        dateObj, level = self.getTool(tool_id)
         date = str(dateObj)[:7]
-
         HTMLDetails = {
             CertificationLevels.NONE:
             '<TD class="clNone"></TD>',
@@ -57,14 +56,16 @@ class ToolUser(object):
             f'<TD class="clInstructor">Instructor<br/>{date}</TD>',
             CertificationLevels.CERTIFIER:
             f'<TD class="clCertifier">Certifier<br/>{date}</TD>'
-        }
+            }
+
         try:
             return HTMLDetails[level]
         except KeyError:  # pragma: no cover
             return "Key: " + str(level)
 
 
-class Certifications(object):
+class Certifications:
+
     def __init__(self):
         self.levels = {
             CertificationLevels.NONE: 'NONE',
@@ -75,12 +76,7 @@ class Certifications(object):
             CertificationLevels.CERTIFIER: 'CERTIFIER'
         }
 
-    def addTool(self,
-                dbConnection,
-                tool_id,
-                grouping,
-                name,
-                restriction=0,
+    def addTool(self, dbConnection, tool_id, grouping, name, restriction=0,
                 comments=''):  # pragma: no cover
         dbConnection.execute('INSERT INTO tools VALUES(?,?,?,?,?)',
                              (tool_id, grouping, name, restriction, comments))
@@ -95,6 +91,7 @@ class Certifications(object):
                  [13, 4, "Vertical Band Saw"], [14, 4, "Jig Saw"],
                  [15, 5, "CNC router"], [16, 5, "Metal Lathe"],
                  [17, 5, "Table Saw"], [18, 5, "Power Miter Saw"]]
+
         for tool in tools:
             if tool[2] == "Power Miter Saw" or tool[
                     2] == "Table Saw":  # Over 18 only tools
@@ -104,33 +101,29 @@ class Certifications(object):
 
     def migrate(self, dbConnection, db_schema_version):
         if db_schema_version < 8:
-            dbConnection.execute('''CREATE TABLE restrictions
-                                    (id        INTEGER PRIMARY KEY,
-                                     descr     TEXT)''')
-            dbConnection.execute('''INSERT INTO restrictions VALUES(
-                                1, "Over 18 Only")''')
-
-            dbConnection.execute('''CREATE TABLE tools
-                                 (id           INTEGER PRIMARY KEY,
-                                  grouping     TEXT,
-                                  name         TEXT,
-                                  restriction  INTEGER DEFAULT 0,
-                                  comments     TEXT)''')
-
-            dbConnection.execute('''CREATE TABLE certifications
-                                 (user_id       TEXT,
-                                  tool_id       INTEGER,
-                                  certifier_id  TEXT,
-                                  date          TIMESTAMP,
-                                  level         INTEGER default 0)''')
+            query = ("CREATE TABLE restrictions (id INTEGER PRIMARY KEY, "
+                     "descr TEXT);")
+            dbConnection.execute(query)
+            query = "INSERT INTO restrictions VALUES(1, 'Over 18 Only');"
+            dbConnection.execute(query)
+            query = ("CREATE TABLE tools (id INTEGER PRIMARY KEY, "
+                     "grouping TEXT, name TEXT, "
+                     "restriction INTEGER DEFAULT 0, comments TEXT);")
+            dbConnection.execute(query)
+            query = ("CREATE TABLE certifications (user_id TEXT, "
+                     "tool_id INTEGER, certifier_id TEXT, date TIMESTAMP, "
+                     "level INTEGER default 0);")
+            dbConnection.execute(query)
             self.addTools(dbConnection)
+
         if db_schema_version < 16:
             self.addTool(dbConnection, 19, 3, "Grinder")
-            dbConnection.execute(
-                "UPDATE tools SET name='Sander' WHERE id=10")
-            for row in dbConnection.execute(
-                '''SELECT user_id, tool_id, date, level, certifier_id FROM certifications
-                                        WHERE tool_id=10'''):
+            query = "UPDATE tools SET name='Sander' WHERE id=10;"
+            dbConnection.execute(query)
+            query = ("SELECT user_id, tool_id, date, level, certifier_id "
+                     "FROM certifications WHERE tool_id = 10;")
+
+            for row in dbConnection.execute(query):
                 self.addCertification(dbConnection, row[0], 19, row[3], row[2],
                                       row[4])
 
@@ -147,89 +140,96 @@ class Certifications(object):
 
     def addCertification(self, dbConnection, barcode, tool_id, level, date,
                          certifier):
-        # TODO: need to verify that the certifier can indeed certify on this tool
-
-        dbConnection.execute(
-            '''INSERT INTO certifications(user_id, tool_id, certifier_id, date, level)
-                                SELECT ?, ?, ?, ?, ?''',
-            (barcode, tool_id, certifier, date, level))
+        # TODO: need to verify that the certifier can indeed certify
+        # on this tool
+        query = ("INSERT INTO certifications (user_id, tool_id, certifier_id, "
+                 "date, level) SELECT ?, ?, ?, ?, ?;")
+        dbConnection.execute(query, (barcode, tool_id, certifier, date, level))
 
     def getAllUserList(self, dbConnection):
         users = {}
-        for row in dbConnection.execute(
-            '''SELECT user_id, tool_id, date, level, v_current_members.displayName FROM certifications
-                                            INNER JOIN v_current_members ON v_current_members.barcode=user_id
-                                            ORDER BY v_current_members.displayName'''):
+        query = ("SELECT crt.user_id, crt.tool_id, crt.date, crt.level, "
+                 "vcm.displayName FROM certifications AS crt "
+                 "INNER JOIN v_current_members AS vcm "
+                 "ON vcm.barcode = crt.user_id ORDER BY vcm.displayName;")
+
+        for row in dbConnection.execute(query):
             try:
                 users[row[0]].addTool(row[1], row[2], row[3])
             except KeyError:
                 users[row[0]] = ToolUser(row[4], row[0])
                 users[row[0]].addTool(row[1], row[2], row[3])
+
         return users
 
     def getInBuildingUserList(self, dbConnection):
         users = {}
-        for row in dbConnection.execute(
-                '''SELECT user_id, tool_id, date, level, members.displayName FROM certifications
-                                        INNER JOIN members ON members.barcode=user_id
-                                        INNER JOIN visits ON visits.barcode=user_id
-                                        WHERE visits.status="In"
-                                        ORDER BY members.displayName'''):
+        query = ("SELECT c.user_id, c.tool_id, c.date, c.level, m.displayName "
+                 "FROM certifications c "
+                 "INNER JOIN members m ON c.user_id = m.barcode "
+                 "INNER JOIN visits v ON m.barcode = v.barcode "
+                 "WHERE v.status = 'In' ORDER BY m.displayName;")
+
+        for row in dbConnection.execute(query):
             try:
                 users[row[0]].addTool(row[1], row[2], row[3])
             except KeyError:
                 users[row[0]] = ToolUser(row[4], row[0])
                 users[row[0]].addTool(row[1], row[2], row[3])
+
         return users
 
     def getTeamUserList(self, dbConnection, team_id):
-        users = {}
         # This is because SQLITE doesn't support RIGHT JOIN
-        for row in dbConnection.execute(
-            '''
-                SELECT team_members.barcode, displayName, type FROM
-                team_members
-                INNER JOIN members ON members.barcode=team_members.barcode
-                WHERE (team_id == ?)
-                ORDER BY type DESC, displayName ASC
-            ''', (team_id,)):
+        users = {}
+        query = ("SELECT tm.barcode, m.displayName, tm.type "
+                 "FROM team_members tm "
+                 "INNER JOIN members m ON tm.barcode = m.barcode "
+                 "WHERE tm.team_id = ? "
+                 "ORDER BY tm.type DESC, m.displayName ASC;")
+
+        for row in dbConnection.execute(query , (team_id,)):
             users[row[0]] = ToolUser(row[1], row[0])
 
-        for row in dbConnection.execute(
-            '''SELECT user_id, tool_id, date, level, members.displayName FROM certifications
-                                            INNER JOIN members ON members.barcode=user_id
-                                            INNER JOIN team_members ON members.barcode=team_members.barcode
-                                            WHERE team_members.team_id = ?
-                                            ORDER BY team_members.type DESC, members.displayName ASC''',
-                (team_id, )):
+        query = ("SELECT c.user_id, c.tool_id, c.date, c.level, m.displayName "
+                 "FROM certifications c "
+                 "INNER JOIN members m ON c.user_id = m.barcode "
+                 "INNER JOIN team_members tm ON m.barcode = tm.barcode "
+                 "WHERE tm.team_id = ? "
+                 "ORDER BY tm.type DESC, m.displayName ASC;")
+
+        for row in dbConnection.execute(query, (team_id, )):
             try:
                 users[row[0]].addTool(row[1], row[2], row[3])
             except KeyError:
                 users[row[0]] = ToolUser(row[4], row[0])
                 users[row[0]].addTool(row[1], row[2], row[3])
-        # This is because SQLITE doesn't support RIGHT JOIN
 
         return users
 
     def getUserList(self, dbConnection, user_id):
         users = {}
-        for row in dbConnection.execute(
-            '''SELECT user_id, tool_id, date, level, members.displayName FROM certifications
-                                            INNER JOIN members ON members.barcode=user_id
-                                            WHERE user_id = ?''',
-                (user_id, )):
+        query = ("SELECT c.user_id, c.tool_id, c.date, c.level, m.displayName "
+                 "FROM certifications c WHERE c.user_id IN ("
+                 "SELECT barcode FROM members WHERE barcode = ?);")
+
+        for row in dbConnection.execute(query, (user_id,)):
             try:
                 users[row[0]].addTool(row[1], row[2], row[3])
             except KeyError:
                 users[row[0]] = ToolUser(row[4], row[0])
                 users[row[0]].addTool(row[1], row[2], row[3])
+
         return users
 
     def getAllTools(self, dbConnection):
         tools = []
-        for row in dbConnection.execute(
-                'SELECT id, name, grouping FROM tools ORDER BY grouping, id ASC', ()):
+        query = ("SELECT id, name, grouping FROM tools "
+                 "ORDER BY grouping, id ASC;")
+
+        for row in dbConnection.execute(query, ()):
             tools.append([row[0], row[1], row[2]])
+
         return tools
 
     def getToolsFromList(self, dbConnection, inputStr):
@@ -240,21 +240,24 @@ class Certifications(object):
         for tool in tools:
             if str(tool[0]) in inputTools:
                 newToolList.append(tool)
+
         return newToolList
 
     def getListCertifyTools(self, dbConnection, user_id):
         tools = []
+        query = ("SELECT c.tool_id, t.name FROM certifications c "
+                 "INNER JOIN tools t ON c.tool_id = t.id "
+                 "WHERE c.user_id = ? AND c.level >= ? ORDER BY t.name ASC;")
+
         for row in dbConnection.execute(
-            '''SELECT id, name FROM tools
-                     INNER JOIN certifications ON certifications.tool_id = id
-                     WHERE user_id = ? AND level >= ? ORDER BY name ASC''',
-                (user_id, CertificationLevels.CERTIFIER)):
+            query, (user_id, CertificationLevels.CERTIFIER)):
             tools.append([row[0], row[1]])
+
         return tools
 
     def getToolName(self, dbConnection, tool_id):
-        data = dbConnection.execute('SELECT name FROM tools WHERE id == ?',
-                                    (tool_id, )).fetchone()
+        query = "SELECT name FROM tools WHERE id == ?;"
+        data = dbConnection.execute(query, (tool_id,)).fetchone()
         return data[0]
 
     def getLevelName(self, level):
@@ -262,8 +265,8 @@ class Certifications(object):
 
     def emailCertifiers(self, name, toolName, levelDescription, certifierName):
         emailAddress = "shopcertifiers@theforgeinitiative.org"
-        msg = f"{name} was just certified as {levelDescription} on {toolName} by {certifierName}!!"
-
-        utils.sendEmail("Shop Certifiers", emailAddress,
-                        "New Certification", msg)
+        msg = (f"{name} was just certified as {levelDescription} on "
+               f"{toolName} by {certifierName}!!")
+        utils.sendEmail("Shop Certifiers", emailAddress, "New Certification",
+                        msg)
         return ''
--- src/customReports.py
+++ src/customReports.py
@@ -1,28 +1,31 @@
+# -*- coding: utf-8 -*-
+
 import sqlite3
 import os
 
 
 class CustomReports:
-    def __init__(self, database):
-        self.database = database
+
+    def __init__(self, db_fullpath):
+        self.db_fullpath = db_fullpath
 
     def migrate(self, dbConnection, db_schema_version):
         if db_schema_version < 7:
-            dbConnection.execute('''CREATE TABLE reports
-                                 (report_id INTEGER PRIMARY KEY,
-                                 name TEXT UNIQUE,
-                                 sql_text TEXT,
-                                 parameters TEXT,
-                                 active INTEGER default 1)''')
+            query = ("CREATE TABLE reports (report_id INTEGER PRIMARY KEY, "
+                     "name TEXT UNIQUE, sql_text TEXT, parameters TEXT, "
+                     "active INTEGER default 1);")
+            dbConnection.execute(query)
 
     def injectData(self, dbConnection, data):
+        query = "INSERT INTO reports VALUES (?,?,?,'',1);"
+
         for datum in data:
             dbConnection.execute(
-                "INSERT INTO reports VALUES (?,?,?,'',1)",
-                (datum["report_id"], datum["name"], datum["sql_text"]))
+                query, (datum["report_id"], datum["name"], datum["sql_text"]))
 
     def readOnlyConnect(self):
-        return sqlite3.connect('file:' + self.database + '?mode=ro', uri=True)
+        return sqlite3.connect('file:' + self.db_fullpath + '?mode=ro',
+                               uri=True)
 
     def customSQL(self, sql):
         # open as read only
@@ -31,34 +34,43 @@ class CustomReports:
             cur.execute(sql)
             header = [i[0] for i in cur.description]
             rows = [list(i) for i in cur.fetchall()]
-        return (header, rows)
+
+        return header, rows
 
     def customReport(self, report_id):
-        with self.readOnlyConnect() as c:
-            data = c.execute("SELECT * FROM reports WHERE (report_id=?)",
-                             (report_id, )).fetchone()
-            if data:
-                title = data[1]
-                sql = data[2]
-                (header, rows) = self.customSQL(sql)
+        query = "SELECT * FROM reports WHERE (report_id=?);"
 
-                return (title, sql, header, rows)
-        return ("Couldn't find report", "", None, None)
+        with self.readOnlyConnect() as cm:
+            data = cm.execute(query, (report_id,)).fetchone()
+
+        if data:
+            title = data[1]
+            sql = data[2]
+            header, rows = self.customSQL(sql)
+            ret = (title, sql, header, rows)
+        else:
+            ret = ("Couldn't find report", "", None, None)
+
+        return ret
 
     def saveCustomSQL(self, dbConnection, sql, name):
+        query = "INSERT INTO reports VALUES (NULL,?,?,?,1);"
+
         try:
-            dbConnection.execute("INSERT INTO reports VALUES (NULL,?,?,?,1)",
-                                 (name, sql, ""))
-            return ""
+            dbConnection.execute(query, (name, sql, ""))
         except sqlite3.IntegrityError:
-            return "Report already exists with that name"
+            ret = "Report already exists with that name"
+        else:
+            ret = ""
+
+        return ret
 
     def get_report_list(self, dbConnection):
         report_list = []
-        for row in dbConnection.execute(
-                '''SELECT report_id, name
-                                FROM reports
-                                WHERE (active = ?)
-                                ORDER BY name''', (1, )):
+        query = ("SELECT report_id, name FROM reports WHERE (active = ?) "
+                 "ORDER BY name;")
+
+        for row in dbConnection.execute(query, (1,)):
             report_list.append((row[0], row[1]))
+
         return report_list
